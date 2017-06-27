8f755c1cad5d41637b28fd68abea82ee8b5dc34a
souleymaneChs <souleymaneabdellah@gmail.com>
2017-06-27 11:56:31 +0200

    Update Compteur.java

--- Compteur.java
+++ Compteur.java
@@ -1,12 +1,16 @@
 public class Compteur {
 
-	private int compteur = 0
+	private int compteur = 0;
 			
 	public void increment() {
-		this.compteur++
+		this.compteur++;
 	}
 	
 	public int toString() {
-		return this.compteur
+		return this.compteur;
 	}
+
+	public static void toString(){
+		system.out.println(this.compteur);
+	}	
 }
